5c3c36213df1dde30f05fd7a7230cdf69137a9a2
Basil Okache <basilokache@gmail.com>
2025-06-24 21:44:34 +0100

    Updating CRUD methods

--- main.go
+++ main.go
@@ -60,10 +60,27 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var movie Movie
 	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(10000000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
 }
 
 // Update a movie
-func updateMovie(w http.ResponseWriter, r *http.Request) {}
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = params["id"]
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
 
 func main() {
 	r := mux.NewRouter()
