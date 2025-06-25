e235d8f1e3afb0e59061f9fc5060bfcce52e84fe
Basil Okache <basilokache@gmail.com>
2025-06-25 20:28:32 +0100

    Updated CRUD functions

--- main.go
+++ main.go
@@ -72,14 +72,13 @@ func updateMovie(w http.ResponseWriter, r *http.Request) {
 	for index, item := range movies {
 		if item.ID == params["id"] {
 			movies = append(movies[:index], movies[index+1:]...)
-			break
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
 		}
 	}
-	var movie Movie
-	_ = json.NewDecoder(r.Body).Decode(&movie)
-	movie.ID = params["id"]
-	movies = append(movies, movie)
-	json.NewEncoder(w).Encode(movie)
 }
 
 func main() {
@@ -89,10 +88,10 @@ func main() {
 	movies = append(movies, Movie{ID: "2", ISBN: "374382", Title: "The Dark Knight Rises", Director: &Director{FirstName: "Christopher", LastName: "Nolan"}})
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
-	r.HandleFunc("/movies(id)", getMovie).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
-	r.HandleFunc("/movies/(id)", updateMovie).Methods("PUT")
-	r.HandleFunc("/movies(id)", deleteMovie).Methods("DELETE")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Printf("Starting server on port 8000\n")
 	log.Fatal(http.ListenAndServe(":8000", r))
